7e5715556c7de99f690c95aa8b4d3b113a362c7e
JarvisChu <jarvischu@126.com>
2023-06-28 16:59:22 +0800

    import viper and implement config file parsing

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -4,22 +4,38 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jarvischu/hellosvr/app/routers"
+	"github.com/jarvischu/hellosvr/config"
 	"github.com/spf13/cobra"
 )
 
-var Cmd = &cobra.Command{
-	Use:     "server",
-	Short:   "Start a web server",
-	Example: "hellosvr server -c etc/configs.yml",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		return run()
-	},
+var (
+	cfgPath string
+	Cmd     = &cobra.Command{
+		Use:     "server",
+		Short:   "Start a web server",
+		Example: "hellosvr server -c etc/configs.yml",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return setup()
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return run()
+		},
+	}
+)
+
+func init() {
+	Cmd.PersistentFlags().StringVarP(&cfgPath, "config", "c", "", "hellosvr server -c etc/configs.yml")
+}
+
+func setup() error {
+	return config.InitConfig(cfgPath)
 }
 
 func run() error {
-	fmt.Println(" server running...")
+	addr := fmt.Sprintf(":%v", config.GetConfig().App.Port)
+	fmt.Printf("server is running on %v ...\n", addr)
 
 	r := gin.Default()
 	routers.InitRouters(r)
-	return r.Run(":8080")
+	return r.Run(addr)
 }
---
+++ config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+var (
+	config Config
+)
+
+type Config struct {
+	App AppConfig `yaml:"app"`
+}
+
+type AppConfig struct {
+	Port int `yaml:"port"`
+}
+
+func InitConfig(cfgPath string) error {
+	if len(cfgPath) == 0 {
+		fmt.Println("No config file provided, using default settings")
+		defaultSettings()
+		return nil
+	}
+
+	fmt.Printf("Parse config file: %v\n", cfgPath)
+	return parseConfig(cfgPath)
+}
+
+func defaultSettings() {
+	config.App.Port = 8080
+}
+
+func parseConfig(cfgPath string) error {
+	viper.SetConfigFile(cfgPath)
+	if err := viper.ReadInConfig(); err != nil {
+		return err
+	}
+	return viper.Unmarshal(&config)
+}
+
+func GetConfig() *Config {
+	return &config
+}
